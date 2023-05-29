33c938c6f1c7d8967980ccb62348cc2a030ae489
javiergg13 <guerrerogallojavier@gmail.com>
2023-05-29 18:21:27 +0200

    v1.0

---
+++ projects/JuegoTresEnRaya/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import './App.css'
+
+const TURNS = {
+  X: '×',
+  O: 'o'
+}
+
+const Square = ({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : 'gh'}`
+  
+  const handleClick = (index) => {
+    updateBoard(index)
+  }
+  
+  return (
+    <div onClick={handleClick} className = {className}>
+      {children}
+    </div>
+  )
+}
+
+
+
+export function App() {
+  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const updateBoard = (index) => {
+    if (board[index]) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((index) => {
+            return (
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+}
