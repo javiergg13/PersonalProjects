31faa2645bed9aebab7546bb9c7f79fecf1665f2
javiergg13 <guerrerogallojavier@gmail.com>
2023-06-16 11:57:50 +0200

    Componente Image

--- projects/next-13-app/src/app/posts/[id]/comments/page.tsx
+++ projects/next-13-app/src/app/posts/[id]/comments/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const fetchComments = (id) => {
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
@@ -7,7 +9,7 @@ const fetchComments = (id) => {
     .then(response => response.json())
 }
 
-export default async function Post ({ params }) {
+export default async function Post({ params }) {
   const { id } = params
   const comments = await fetchComments(id)
 
@@ -15,6 +17,12 @@ export default async function Post ({ params }) {
     <ul style={{ fontSize: '10px', background: '#444' }}>
       {comments.map(comment => (
         <li key={comment.id}>
+          <Image
+            src={`https://unavatar.io/${comment.email}`}
+            alt={comment.name}
+            width={50}
+            height={50}
+          />
           <h4>{comment.name}</h4>
           <small>{comment.body}</small>
         </li>
